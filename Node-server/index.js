c7c2214c1fa6e15c7f5debe7bdb271f74904fc76
Nikil kumar <kumarnikil220@gmail.com>
2023-01-12 17:51:25 +0530

    message send/receive  feature added

--- Node-server/index.js
+++ Node-server/index.js
@@ -1,40 +1,45 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-const path = require('path');
-const mainfile = path.join(__dirname, '../')
-//mainfile will contain all the required file     
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+const path = require("path");
+const mainfile = path.join(__dirname, "../");
 const port = 8000;
 
 app.use(express.static(mainfile));
-// static is a built-in middleware in express.js. It serve the static files like here mainfile is static file
 
 app.get("/", function (req, res) {
-    res.sendFile(mainfile + '/index.html');
-})
-// Abhi mai isko empty rakh rha hu ,yha users add hote jaayenge baad mai
+  res.sendFile(mainfile + "/index.html");
+});
+
 const activeusers = {};
+
 io.on("connection", (socket) => {
-    socket.on("new_user_joined", (username) => {     //receiving username from client
-        console.log("new user", username);
-        activeusers[socket.id] = username;
-        //pehli id mai pehle username save ho jaayega aur dusri mai dusra mean jitne bhi active users hai 
-        //ye unko array kii form mai save krr ke rakhega 
-        socket.broadcast.emit("User-Joined", username);
-        //jaise he koi user join kiya to ye emit fire ho lega aur humko ye mil jaayega 
-        //isko hum client side se edit karenge   
+  //for user join
+  socket.on("new_user_joined", (username) => {
+    console.log("new user", username);
+    activeusers[socket.id] = username;
+    socket.broadcast.emit("User-Joined", username);
 
-        // for user leave 
-        socket.on("disconnect", () => {
-            console.log("User-left", username);
-        })
+    // for user leave
+    socket.on("disconnect", () => {
+      console.log("left user", username);
+      activeusers[socket.id] = username;
+      socket.broadcast.emit("User-left", username);
+    });
+  });
 
-    })
-})
+  //receiving message from client
+  socket.on("send", (message) => {
+    //console.log(message);
+    //broadcasting the message
+    socket.broadcast.emit("recieve", {
+      message: message,
+      username: activeusers[socket.id],
+    });
+  });
+});
 
 http.listen(port, () => {
-    console.log(`server running at port ${port}`);
-})
-
-
+  console.log(`server running at port ${port}`);
+});
--- client.js
+++ client.js
@@ -1,36 +1,59 @@
 const socket = io();
 // Now we are going to target data like form input,container etc.
-const form = document.getElementById('send'); // have target form
-const messageInput = document.getElementById('send_msg'); // have target send input 
-const messageContainer = document.getElementById('message-box'); // target whole message box 
+const form = document.getElementById("send"); // have target form
+const messageInput = document.getElementById("send_msg"); // have target send input
+const messageContainer = document.getElementById("message-box"); // target whole message box
 
 const append = (message, position) => {
-    const messageElement = document.createElement("div");
-    messageElement.innerText = message;
-    messageElement.classList.add("message");  //for adding class in this div
-    messageElement.classList.add(position);    // yha position kii jagah by default center aa jaayega 
-    messageContainer.appendChild(messageElement);
-}
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.classList.add("message"); //for adding class in this div
+  messageElement.classList.add(position); // yha position kii jagah by default center aa jaayega
+  messageContainer.appendChild(messageElement);
+};
 
-// User join 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const message = messageInput.value;
+  append(`You:${message}`, "right");
+  socket.emit("send", message); //Here send is a event in socket
+  messageInput.value="";
+});
+
+// User join
 const username = prompt("Enter your Username");
-socket.emit("new_user_joined", username);     //sending to sever
+socket.emit("new_user_joined", username); //sending to sever
 
-//agar user joined wala event fire hoga server side se to ye hoga   
+//agar user joined wala event fire hoga server side se to ye hoga
 socket.on("User-Joined", (username) => {
-    append(`${username} joined :)`, "center");
-    //jaise he koi user join karega to server(index.js) User-Joined naam ka event fire karega 
-    //aur client.js uss message ko pakad lega aur wo ek msg append krr dega
-    //ye second param position dii hai mtlb msg kha print karana hai to simply hum isko 
-    //center wale div mai karwa rhe hai jo hai mere index.html file mai
-})
-
+  append(`${username} joined :)`, "center");
+  //jaise he koi user join karega to server(index.js) User-Joined naam ka event fire karega
+  //aur client.js uss message ko pakad lega aur wo ek msg append krr dega
+  //ye second param position dii hai mtlb msg kha print karana hai to simply hum isko
+  //center wale div mai karwa rhe hai jo hai mere index.html file mai
+});
+//recieving message from server
+socket.on("recieve",(data) => {
+  append(`${data.username}:${data.message}`,"left");
+  //left is a class jiske through hum server wale msg ko left side mai rakhenge
+});
 
 //agar user joined wala event fire hoga server side se to ye hoga
 socket.on("User-left", (username) => {
-    append(`${username} Left :(`, "center");
-    //jaise he koi user join karega to server(index.js) User-Joined naam ka event fire karega 
-    //aur client.js uss message ko pakad lega aur wo ek msg append krr dega
-    //ye second param position dii hai mtlb msg kha print karana hai to simply hum isko 
-    //center wale div mai karwa rhe hai jo hai mere index.html file mai
-})
+  append(`${username} Left :(`, "center");
+  //jaise he koi user join karega to server(index.js) User-Joined naam ka event fire karega
+  //aur client.js uss message ko pakad lega aur wo ek msg append krr dega
+  //ye second param position dii hai mtlb msg kha print karana hai to simply hum isko
+  //center wale div mai karwa rhe hai jo hai mere index.html file mai
+});
+
+
+
+
+
+
+
+/*******************************************NOTES**********************************/
+/**Abhi ek issue hai kii jaise he hum send button prr click krr rhe hai to ye pure page ko
+ * reload krr rha hai to humko iske default behavior ko rokna padega
+ */
